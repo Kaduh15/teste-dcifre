3da8ff65aa24898db75e62f9b4535e99d364389f
Kadu <kadu.silva2014@gmail.com>
2025-02-14 13:47:28 -0300

    feat: cria rota que busca empresa por id

--- routes_empresa.py
+++ routes_empresa.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from connection_db import get_db
 from models import Empresa
@@ -10,12 +10,21 @@ router = APIRouter(prefix="/empresas", tags=["Empresas"])
 
 @router.post("/", response_model=EmpresaResponse)
 def criar_empresa(empresa: EmpresaCreate, db: Session = Depends(get_db)):
-    nova_empresa = Empresa(**empresa.dict())
+    nova_empresa = Empresa(**empresa.model_dump())
     db.add(nova_empresa)
     db.commit()
     db.refresh(nova_empresa)
     return nova_empresa
 
+
 @router.get("/", response_model=List[EmpresaResponse])
 def listar_empresas(db: Session = Depends(get_db)):
     return db.query(Empresa).all()
+
+
+@router.get("/{empresa_id}", response_model=EmpresaResponse)
+def obter_empresa(empresa_id: int, db: Session = Depends(get_db)):
+    empresa = db.query(Empresa).filter(Empresa.id == empresa_id).first()
+    if not empresa:
+        raise HTTPException(status_code=404, detail="Empresa não encontrada")
+    return empresa
